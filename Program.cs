0931ab8f3cd0fef506946d1506f05b47eb1af394
Zet1987 <m.a.chentsova@yandex.ru>
2023-02-06 02:54:42 +0500

    finish

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите массив из слов");
+string one = Console.ReadLine()!;
+
+Console.WriteLine("Введите массив из слов");
+string two = Console.ReadLine()!;
+
+Console.WriteLine("Введите массив из слов");
+string three = Console.ReadLine()!;
+
+Console.WriteLine("Введите массив из слов");
+string four = Console.ReadLine()!;
+
+string [] Array  = {one, two, three, four};
+int count=0;
+for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+                count++;
+        }
+    }
+
+string [] Arrayfind = new string[Array.Length];
+Console.WriteLine(count);
+Console.WriteLine(String.Join("+ ", Arrayfind));
+Console.WriteLine("Первый массив");
+
+Console.WriteLine(String.Join(", ", Array));
+for (int j = 0; j < Array.Length; j++)
+    {
+        if (Array[j].Length <= 3)
+        {
+                Arrayfind[j] = Array[j];
+        }
+   else
+   {
+    Arrayfind[j] = "";
+   }
+    }
+Console.WriteLine("Второй массив");
+Console.WriteLine(String.Join(" ", Arrayfind));
